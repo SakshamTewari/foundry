d7d8e6c20503391ccc6b2445f53c90bdb1b9f926
Saksham Tewari <sakshamtewari3@gmail.com>
2025-01-30 22:31:47 +0530

    "vm.prank" to change msg.sender

--- basics/test/SakshamCoin.t.sol
+++ basics/test/SakshamCoin.t.sol
@@ -16,4 +16,19 @@ contract TestSakshamCoin is Test {
         c.mint(address(this), 100);
         assertEq(c.balanceOf(address(this)), 100, "ok");
     }
+
+    function testTreansfer() public{
+        c.mint(address(this), 100);
+        c.transfer(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 100);
+
+        assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4), 100, "ok");
+
+        // vm.prank (to change msg.sender while test)
+        vm.prank(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        c.transfer(address(this), 100);
+
+        assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4), 0, "ok");
+        assertEq(c.balanceOf(address(this)), 100,"ok");
+
+    }
 }
