d5f6cd4dd23b65f88271b74331d914fe75e01f59
Saksham Tewari <sakshamtewari3@gmail.com>
2025-02-04 22:44:30 +0530

    'BridgeETH' initialize

---
+++ bridges/src/BridgeETH.sol
@@ -0,0 +1,15 @@
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BridgeETH is Ownable{
+    constructor() Ownable(){
+
+    }
+
+    function deposit() public {
+
+    }
+
+    function withdraw() public {
+
+    }
+}
