9c981a4bdc8e1520d56036d0eac39c669cc87fce
Saksham Tewari <sakshamtewari3@gmail.com>
2025-02-04 22:43:58 +0530

    'BridgeChain' initialize

---
+++ bridges/src/BridgeChain.sol
@@ -0,0 +1,19 @@
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BridgeChain is Ownable {
+    constructor() Ownable(msg.sender){
+
+    }
+
+    function withdraw() public {
+
+    }
+
+    function butn() public {
+
+    }
+
+    function depositHappenedOnETH() public {
+        
+    }
+}
