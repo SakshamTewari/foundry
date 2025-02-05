57a8f846b88c31bc288cf83abadf66c3f2cf0231
Saksham Tewari <sakshamtewari3@gmail.com>
2025-02-05 23:45:38 +0530

    'pendingBalance'mapping, 'Deposit' event

--- bridges/src/BridgeETH.sol
+++ bridges/src/BridgeETH.sol
@@ -1,15 +1,28 @@
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BridgeETH is Ownable{
-    constructor() Ownable(){
+    uint public balance;
+    address public tokenAddress;
 
+    // To trigger the 'Node.js' process to communicate with the other chain
+    event Deposit(address indexed depositor, uint amount);
+
+    mapping(address => uint) public pendingBalance;
+    constructor(address _tokenAddress) Ownable(msg.sender){
+        tokenAddress = _tokenAddress;
     }
 
-    function deposit() public {
+    function deposit(IERC20 _tokenAddress, uint _amount) public {
 
+        emit Deposit(msg.sender, _amount);
     }
 
-    function withdraw() public {
+    function withdraw(IERC20 _tokenAddress, uint _amount) public {
+        pendingBalance[msg.sender] -= _amount;
+    }
 
+    function burnedOnOtherChain(address userAccount, uint _amount) public onlyOwner {
+        // as user deposits/burn WSaksham on other chain, we increase the balance here
+        pendingBalance[userAccount] += _amount;
     }
 }
